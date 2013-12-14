4407cb26ce363460aa0d065b3ffe182a399db5aa
Aditya Padhye <padhyeav@gmail.com>
2013-12-14 22:03:28 +1030

    added nav menu to header

---
+++ functions.php
@@ -0,0 +1,8 @@
+
+<?php
+//create nav menu
+if (function_exists('register_nav_menus')) (
+    register_nav_menu( array( 'primary' => 'Header Navigation' ) )
+)
+
+?>
--- header.php
+++ header.php
@@ -3,22 +3,27 @@
     <head>
         <!--This is a comment. Comments are not displayed in the browser-->
         <title>    
-            <?php 
-                wp_title('|',true,'right');   //seperator, echo - yes,  title to the right of the seperator  
-                bloginfo('name');   // grab name of the website
+            <?php
+            wp_title('|', true, 'right');   //seperator, echo - yes,  title to the right of the seperator  
+            bloginfo('name');   // grab name of the website
             ?>
         </title>
-        <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url');?>/style.css">
-        <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url');?>/skeleton.css">
-        <?php wp_head();?>  <!-- checks if theme supports hook and bring in a lot of information -->
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/style.css">
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/skeleton.css">
+        <?php wp_head(); ?>  <!-- checks if theme supports hook and bring in a lot of information -->
     </head>
-    
-    
+
+
     <body>
         <div class="container">
-            <div class="five columns clearfix"> 
-                <a href="<?php echo get_option('home');?>"><img src ="<?php bloginfo('template_url');?>/img/logo.png" title="bloginfo('title')"></a>
-            </div>
+            <header>    <!-- HTML5 tag for any header -->
+                <div class="five columns clearfix"> 
+                    <a href="<?php echo get_option('home'); ?>"><img src ="<?php bloginfo('template_url'); ?>/img/logo.png" title="bloginfo('title')"></a>
+                </div>
+            </header>
+            <div class="sixteen columns nav-bar"> <!-- how does this work -->
+                <?php wp_nav_menu( array( 'container-class' => 'main-nav', 'container' => 'nav') ); ?>   
+            </div>               
         </div>        
     </body>
 </html>
